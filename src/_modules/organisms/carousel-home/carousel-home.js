d14eee01a1ab18a0981ced32cbe883ae64e7bc91
josmanmx <encpagjos@gmail.com>
2018-07-08 20:25:35 -0400

    Added  content: - carousel (home) - services (home) - contact-form (home) - testimonials (home)

---
+++ src/_modules/organisms/carousel-home/carousel-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var CarouselHome = function() {
+  this.name = 'carousel-home';
+  console.log('%s module', this.name.toLowerCase());
+};
+
+module.exports = CarouselHome;
---
+++ src/_modules/organisms/form-home/form-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var FormHome = function() {
+  this.name = 'form-home';
+  console.log('%s module', this.name.toLowerCase());
+};
+
+module.exports = FormHome;
